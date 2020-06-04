00f47288fba443982c1f13bbb08c57b3448ccfa8
Ariel Zingman <arielzingman@Ariels-MacBook-Pro.local>
2020-06-04 21:09:44 +0300

    almost a square

--- include/phyisicsObj.h
+++ include/phyisicsObj.h
@@ -3,16 +3,28 @@
 #include <SFML/Graphics.hpp>
 #include <box2d/box2d.h>
 
-class phyisicsObj
+class PhyisicsObj
 {
 private:
 
     
+    sf::Vector2f m_loc;
+    sf::Vector2f m_dimensions;
 
+    b2Body* m_body = nullptr;
+    b2Fixture* m_fixture = nullptr;
 
+    //sf::Color m_color;
    
 public:
-    phyisicsObj();
-    ~phyisicsObj();
-};
 
+    void init(b2World* world, const sf::Vector2f& position, const sf::Vector2f& dimensions); //, sf::Color color) 
+
+    PhyisicsObj();
+    ~PhyisicsObj();
+
+    b2Body* getBody() const { return m_body; }
+    b2Fixture* getFixture() const { return m_fixture; }
+    const sf::Vector2f& getDimensions() const { return m_dimensions; }
+    //const  Bengine::ColorRGBA8& getColor() const { return m_color; }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,38 @@
 #include <SFML/Graphics.hpp>
 #include <box2d/box2d.h>
+#include <PhyisicsObj.h>
+#include <vector>
 
 int main()
 {
-    sf::CircleShape();
-    
-    b2World world(b2Vec2(0,-9.81));
-   
+
+    std::vector<PhyisicsObj> boxes;
+
+    auto world = std::make_unique<b2World>(b2Vec2(0,-9.81));
+
+    b2BodyDef groundBodyDef;
+    groundBodyDef.position.Set(0.0f,-10.0f);
+    b2Body* body = world->CreateBody(&groundBodyDef);
+    b2PolygonShape boxShape;
+    boxShape.SetAsBox(50.0f,10.0f);
+    body->CreateFixture(&boxShape, 0.0f); //0 density coz its static
+
+    PhyisicsObj box;
+    box.init(world.get(), sf::Vector2f(0.0f,14.0f), sf::Vector2f(15.0f,15.0f));
+    boxes.push_back(box);
+
+
+    sf::RectangleShape rect;
+    rect.setPosition(sf::Vector2f(box.getBody()->GetPosition().x, box.getBody()->GetPosition().y));
+    rect.setFillColor(sf::Color::White);
+    rect.setSize(sf::Vector2f(20.0f,20.0f));
+
     auto window = sf::RenderWindow(sf::VideoMode(300, 300), "Example");
 
     while (window.isOpen())
     {
         window.clear();
+        window.draw(rect);
         window.display();
 
         
