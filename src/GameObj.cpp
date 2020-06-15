00f97811ac0d3812513369bbfdb3db463e9be71a
Ariel Zingman <arielzingman@Ariels-MacBook-Pro.local>
2020-06-15 12:40:17 +0300

    added gmOb

---
+++ src/GameObj.cpp
@@ -0,0 +1,21 @@
+#include "GameObj.h"
+
+GameObj::GameObj(const sf::Vector2f& center, const sf::Vector2f& size, sf::Texture* texture, bool dynamic, bool movable, b2World &world)
+	:BaseImg(center, size, texture), m_phyObj(world, center, size, dynamic), m_movable(movable)
+{
+	static int ID = 0;
+	I
+}
+
+void GameObj::draw (sf::RenderWindow& win)
+{
+	updateLoc();
+	BaseImg::draw(win);
+}
+
+void GameObj::updateLoc()
+{
+	auto pos = m_phyObj.getPosition();
+	BaseImg::setLocation(sf::Vector2f(pos.x,pos.y));
+	BaseImg::setRotation(m_phyObj.getAngle());
+}
