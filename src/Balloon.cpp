9cc6c6636a2b428640a98899b206e50fc3698864
itzhakva <itzhakva@edu.hac.ac.il>
2020-06-18 12:41:21 +0300

    opening push branch

--- src/Balloon.cpp
+++ src/Balloon.cpp
@@ -5,5 +5,5 @@ Balloon::Balloon(const sf::Vector2f& center, bool movable, b2World &world)
     :GameObj(center, ResourceManager::instance().getTexture(balloon)->getSize(), ResourceManager::instance().getTexture(balloon), true, movable, world, balloon)
 {
     setGravityScale(-0.3);
-    //updateTexture(sf::IntRect(sf::Vector2i(9,12),sf::Vector2i(32,47)));
+    //updateTexture(sf::IntRect(sf::Vector2i(9,12),sf::Vector2i(32,47))); itzhak
 }
