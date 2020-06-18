6ac87f93b06637df29826516f8bd50e5391efb52
itzhakva <itzhakva@edu.hac.ac.il>
2020-06-18 12:14:03 +0300

    update win changes

--- src/ResourceManager.cpp
+++ src/ResourceManager.cpp
@@ -33,20 +33,20 @@ ResourceManager::ResourceManager()
 {
 	//---------- textures -------windows---
 
-	//   loadTexture("balloonPopping.png", GameObject_t::balloon);
-	//   loadTexture("basketBall.png", GameObject_t::basketBall);
-	//   loadTexture("baseBall.png", GameObject_t::baseBall);
-	//   loadTexture("bowlingBall.png", GameObject_t::bowlingBall);
-	//   loadTexture("conveyor.png", GameObject_t::conveyor);
-	//   loadTexture("bricks.png", GameObject_t::brickWallH);
-	//   loadTexture("bricksV.png", GameObject_t::brickWallV);
-	//   loadTexture("worker.jpg", GameObject_t::play);
-	//   loadTexture("arrows.png", GameObject_t::arrows);
-	//   loadTexture("startButton.png", GameObject_t::start_button);
-	//   loadTexture("exitButton.png", GameObject_t::exit_button);
-	//   loadTexture("The_Incredible_Machine.png", GameObject_t::background);
+	   loadTexture("balloonPopping.png", GameObject_t::balloon);
+	   loadTexture("basketBall.png", GameObject_t::basketBall);
+	   loadTexture("baseBall.png", GameObject_t::baseBall);
+	   loadTexture("bowlingBall.png", GameObject_t::bowlingBall);
+	   loadTexture("conveyor.png", GameObject_t::conveyor);
+	   loadTexture("brickH.png", GameObject_t::brickWallH);
+	   loadTexture("brickV.png", GameObject_t::brickWallV);
+	   loadTexture("play.png", GameObject_t::play);
+	   loadTexture("arrows.png", GameObject_t::arrows);
+	   loadTexture("startButton.png", GameObject_t::start_button);
+	   loadTexture("exitButton.png", GameObject_t::exit_button);
+	   loadTexture("The_Incredible_Machine.png", GameObject_t::background);
 
-	loadTexture("build/balloonPopping.png", GameObject_t::balloon);
+	/*loadTexture("build/balloonPopping.png", GameObject_t::balloon);
 	loadTexture("build/basketBall.png", GameObject_t::basketBall);
 	loadTexture("build/baseBall.png", GameObject_t::baseBall);
 	loadTexture("build/bowlingBall.png", GameObject_t::bowlingBall);
@@ -57,13 +57,16 @@ ResourceManager::ResourceManager()
 	loadTexture("build/arrows.png", GameObject_t::arrows);
 	loadTexture("build/startButton.png", GameObject_t::start_button);
 	loadTexture("build/exitButton.png", GameObject_t::exit_button);
-	loadTexture("build/The_Incredible_Machine.png", GameObject_t::background);
+	loadTexture("build/The_Incredible_Machine.png", GameObject_t::background);*/
 
 
 	//
 
+	//---------- font ---mac-------
+	/*loadFont("build/kongtext.ttf", Font::kongtext);*/
+
 	//---------- font ---win-------
-	loadFont("build/kongtext.ttf", Font::kongtext);
+	loadFont("kongtext.ttf", Font::kongtext);
 
 	//---------- font ----------
 	//loadFont("/Users/arielzingman/OOP2/EX5TheIncredibleMachine/TheIncredibleMachine/resources/Amadeus.ttf", Font::ariel);
